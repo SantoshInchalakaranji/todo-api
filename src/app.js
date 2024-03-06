0a82c237df9571f5c861a27675fb7ecefc43e413
Santosh <santoshinchalakaranje@gmail.com>
2024-03-06 16:44:42 +0530

    app update

--- src/app.js
+++ src/app.js
@@ -20,4 +20,12 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
 module.exports = app;
