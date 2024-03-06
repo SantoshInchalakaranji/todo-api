ede4782006984723c93caf79b931676e3fc07523
Santosh <santoshinchalakaranje@gmail.com>
2024-03-06 16:29:38 +0530

    app update

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const todoRoutes = require('./routes/todoRoutes');
 
 const apiKey = '1234567890';
 
-//middleware
+
 app.use(logger);
 app.use(authenticate(apiKey));
 app.use(express.json());
 
-// Routes
+
 app.use('/api/todos', todoRoutes);
 
-// Error handling middleware
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal Server Error' });
